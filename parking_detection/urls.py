6acdaccc9e6809b1e45c266eed11f9665bddc953
vjani99 <vjani99@gmail.com>
2022-03-09 14:21:48 -0500

    Made Spot branding link to homepage

--- parking_detection/urls.py
+++ parking_detection/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index),
+    path('', views.index, name='index'),
     path('harris-commons', views.parking_lot, name='parking_lot')
 ]
