326bb9a9e112402032b2b61bfeb8d8e196c8f27d
Nicolas Ayala <nicoayala0@gmail.com>
2022-03-20 17:17:13 -0400

    CSS and HTML functions

--- parking_detection/urls.py
+++ parking_detection/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('harris-commons', views.parking_lot, name='parking_lot')
+    path('harris-commons', views.parking_lot, name='parking_lot'),
+    path('about_us', views.about_us, name='about_us'),
+    path('contact_us', views.contact_us, name='contact_us')
 ]
