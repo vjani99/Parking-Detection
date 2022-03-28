36165cdafe23bc1fd9dcdda457883cc6554f5bcc
vjani99 <vjani99@gmail.com>
2022-03-28 16:28:26 -0400

    Added running clock in frontend and map-data api endpoint in backend

--- parking_detection/urls.py
+++ parking_detection/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('harris-commons', views.parking_lot, name='parking_lot')
+    path('harris-commons', views.parking_lot, name='parking_lot'),
+    path('map-data', views.model_output, name='map_data'),
 ]
--- parking_detection/views.py
+++ parking_detection/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt 
 
 # Create your views here.
 def index(request):
@@ -7,3 +9,16 @@ def index(request):
 
 def parking_lot(request):
     return render(request, 'parking_detection/parking_lot.html')
+
+@csrf_exempt
+def model_output(request):
+    if request.method == "PUT":
+        nn_output = {
+            "zone1": 1,
+            "zone2": 0,
+            "zone3": 1,
+            "zone4": 0, 
+        }
+        return JsonResponse(nn_output)
+    else:
+        return JsonResponse({})
